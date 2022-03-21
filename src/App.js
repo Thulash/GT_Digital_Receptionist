f9d3ea5dbff75143f95cd57b392ba959b55a950b
Thulashini <Thula@LAPTOP-0GG9R0LO.(none)>
2022-03-21 14:20:04 +0530

    interface base files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Chat, { Bubble, useMessages } from "@chatui/core";
+import "@chatui/core/dist/index.css";
+import "./index.css";
+import SplitPane from "react-split-pane";
+//import ChatSDK from "@chatui/core";
+//import './chatui-theme.css';
 
-function App() {
+const initialMessages = [
+  {
+    type: 'text',
+    content: { text: 'Welcome to Digital Receptionist Sam' },
+    user: { avatar: 'http://127.0.0.1:5500/gui/src/logo.svg'}
+  },
+];
+
+export default function App() {
+  const { messages, appendMsg, setTyping } = useMessages(initialMessages);
+
+  function handleSend(type, val) {
+    if (type === "text" && val.trim()) {
+      appendMsg({
+        type: "text",
+        content: { text: val },
+        position: "right",
+        user :{ avatar: 'http://127.0.0.1:5500/gui/src/user.svg' }
+      });
+
+      setTyping(true);
+
+      setTimeout(() => {
+        appendMsg({
+          type: "text",
+          content: { text: "YaYa" },
+          user: { avatar: 'http://127.0.0.1:5500/gui/src/logo.svg' },
+        });
+      }, 1000);
+    }
+  }
+
+  function renderMessageContent(msg) {
+    const { content } = msg;
+    return <Bubble content={content.text} />;
+  }
+/*
+  new ChatSDK({
+    config: {
+      toolbar: [
+        {
+          type: 'speech',
+          icon: 'mic',
+          title: '语音输入'
+        }
+      ]
+    },
+    handlers: {
+      onToolbarClick: function (item, ctx) {
+        if (item.type === 'speech') {
+          // 这里改成 App 提供的 bridge 方法
+          nativeInvoke('speech', (text) => {
+            if (text) {
+              // 通过 setText 更新输入框内容
+              bot.appRef.current.setText(text);
+            }
+          });
+        }
+      }
+    }
+  });
+*/
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SplitPane split="vertical" size={950}>
+      <div>Digital Human</div>
+    <SplitPane split='vertical' size={500}>
+    <Chat
+      locale="en-US"
+      navbar={{ title: "Digital Receptionist Sam" }}
+      messages={messages}
+      renderMessageContent={renderMessageContent}
+      onSend={handleSend}  
+    />
+    </SplitPane>
+    </SplitPane>
+    
   );
 }
-
-export default App;
